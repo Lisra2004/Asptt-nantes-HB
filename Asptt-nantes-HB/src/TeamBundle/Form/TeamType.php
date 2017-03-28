2b3776b0996362397f1a462e71fc6a47cf309884
TournayG <tournayg@gmail.com>
2017-03-28 21:27:42 +0200

    CRUD TeamBundle Team & views created

---
+++ Asptt-nantes-HB/src/TeamBundle/Form/TeamType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace TeamBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TeamType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('teamName');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'TeamBundle\Entity\Team'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'teambundle_team';
+    }
+
+
+}
